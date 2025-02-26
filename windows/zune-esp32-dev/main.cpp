67709a646c69d112b4870961ffdf5419388cc0ec
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-02-27 01:09:04 +0500

    chore: test raylib code

--- windows/zune-esp32-dev/main.cpp
+++ windows/zune-esp32-dev/main.cpp
@@ -6,7 +6,7 @@ int main(void)
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "ZUNE ESP32-S3");
     // TTF font : Font data and atlas are generated directly from TTF
     // NOTE: We define a font base size of 32 pixels tall and up-to 250 characters
-    Font font = LoadFontEx("resources/droidsans.ttf", 100, 0, 250);
+    Font font = LoadFontEx("resources/fonts/droidsans.ttf", 100, 0, 250);
 
     while (!WindowShouldClose())
     {
