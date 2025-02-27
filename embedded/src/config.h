847e60723883842dbdf02769eb319fd93c6db2db
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-02-28 00:58:44 +0500

    feat: implement I2S Speaker and Microphone

---
+++ embedded/src/config.h
@@ -0,0 +1,83 @@
+#include <Arduino.h>
+
+#include <SPI.h>
+#include <TFT_eSPI.h>
+
+#include <TouchScreen.h>
+
+#include "AudioTools.h"
+
+#include "FS.h"
+#include "SD.h"
+#include "SPI.h"
+#include "helpers.h"
+#include "Audio.h"
+
+// Include I2S driver
+#include <driver/i2s.h>
+
+int sck = 36;
+int miso = 37;
+int mosi = 35;
+int cs = 39;
+
+#define YP 14 // must be an analog pin, use "An" notation!
+#define XM 13 // must be an analog pin, use "An" notation!
+#define YM 5  // can be a digital pin
+#define XP 4  // can be a digital pin
+
+TouchScreen ts = TouchScreen(XP, YP, XM, YM, 240);
+TSPoint p;
+
+TFT_eSPI tft = TFT_eSPI();
+
+TaskHandle_t Task1;
+
+int x, y;
+int targetFPS = 30;
+
+// I2S Connections for Speaker
+#define I2S_DOUT 40
+#define I2S_BCLK 41
+#define I2S_LRC 42
+// Create Audio object
+Audio audio;
+
+// Connections to INMP441 I2S microphone
+#define I2S_WS 0
+#define I2S_SD 48
+#define I2S_SCK 47
+
+// Use I2S Processor 0
+#define I2S_PORT I2S_NUM_1
+
+// Define input buffer length
+#define bufferLen 1024
+int16_t sBuffer[bufferLen];
+
+// Set up I2S Processor configuration
+const i2s_config_t i2s_config = {
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 16000,
+    .bits_per_sample = i2s_bits_per_sample_t(16),
+    .channel_format = I2S_CHANNEL_FMT_ALL_RIGHT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = bufferLen,
+    .use_apll = false};
+
+// Set I2S pin configuration
+const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = -1,
+    .data_in_num = I2S_SD};
+
+bool recording = false;
+unsigned long runtime;
+
+I2SStream i2s_mic;
+File audioFile;
+EncodedAudioStream out(&audioFile, new WAVEncoder);
+StreamCopy copier(out, i2s_mic);
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,31 +1,4 @@
-#include <Arduino.h>
-
-#include <SPI.h>
-#include <TFT_eSPI.h>
-
-#include <TouchScreen.h>
-
-#include "FS.h"
-#include "SD.h"
-#include "SPI.h"
-#include "helpers.h"
-
-// Uncomment and set up if you want to use custom pins for the SPI communication
-#define REASSIGN_PINS
-int sck = 36;
-int miso = 37;
-int mosi = 35;
-int cs = 39;
-
-#define YP 14 // must be an analog pin, use "An" notation!
-#define XM 13 // must be an analog pin, use "An" notation!
-#define YM 5  // can be a digital pin
-#define XP 4  // can be a digital pin
-
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 240);
-TSPoint p;
-
-TFT_eSPI tft = TFT_eSPI();
+#include "config.h"
 
 void readTouch()
 {
@@ -38,11 +11,50 @@ void readTouch()
     pinMode(XP, OUTPUT);
 }
 
+void Task1code(void *parameter)
+{
+    for (;;)
+    {
+        audio.loop();
+    }
+}
+
 void setup(void)
 {
 
     Serial.begin(115200);
     Serial.println("starting");
+    AudioLogger::instance().begin(Serial, AudioLogger::Info); // AudioLogger set to Debug mode
+    delay(1000);
+
+    // configure I2S stream
+    Serial.println("starting I2S...");
+    i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+    i2s_set_pin(I2S_PORT, &pin_config);
+    auto cfg = i2s_mic.defaultConfig(RX_MODE);
+    cfg.i2s_format = I2S_STD_FORMAT; // or try with I2S_LSB_FORMAT
+    // cfg.i2s_format = I2S_LSB_FORMAT; // or try with
+    cfg.bits_per_sample = 16;
+    cfg.channels = 2;
+    cfg.sample_rate = 16000;
+    cfg.pin_ws = I2S_WS;   // WS
+    cfg.pin_bck = I2S_SCK; // SCK
+    cfg.pin_data = I2S_SD; // SD
+    cfg.port_no = I2S_PORT;
+    i2s_mic.begin(cfg);
+
+    // Configure out stream
+    auto cfg_out = out.defaultConfig();
+    cfg_out.bits_per_sample = 16;
+    cfg_out.channels = 2;
+    cfg_out.sample_rate = 16000;
+    out.begin(cfg_out);
+
+    i2s_start(I2S_PORT);
+
+    copier.setCheckAvailableForWrite(false);
+
+    delay(500);
 
     SPI.begin(sck, miso, mosi, cs);
     if (!SD.begin(cs))
@@ -93,11 +105,49 @@ void setup(void)
     tft.println("calibration run");
     delay(1000);
     tft.fillScreen(TFT_BLACK);
+
+    // // Setup I2S
+    // audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+
+    // // Set Volume
+    // audio.setVolume(21);
+
+    // Open music file
+    // audio.connecttoFS(SD, "/test.mp3");
+
+    // Create a task to run the audio loop
+    // xTaskCreatePinnedToCore(
+    //     Task1code, /* Task function. */
+    //     "Task1",   /* name of task. */
+    //     10000,     /* Stack size of task */
+    //     NULL,      /* parameter of the task */
+    //     0,         /* priority of the task */
+    //     &Task1,    /* Task handle to keep track of created task */
+    //     0);        /* pin task to core 0 */
 }
 
-int x, y;
 void loop()
 {
+
+    runtime = millis();
+
+    if (!recording)
+    {
+        audioFile = SD.open("/record.wav", FILE_WRITE); // Open a new file on the SD card for writing to
+        copier.begin(out, i2s_mic);                     // Configure copier input and output
+        Serial.println("RECORDING");
+        recording = true; // Makes sure this only runs once
+    }
+
+    if (runtime > 10000)
+    {
+        copier.end();
+        audioFile.close(); // Closes the file after 5 seconds
+        Serial.println("FILE CLOSED");
+    }
+    else
+        copier.copy(); // Copy sound from I2S to the audio file
+
     readTouch();
 
     x = (int)((float)(p.x + 2555) / 9.48);
@@ -120,4 +170,10 @@ void loop()
         tft.fillScreen(TFT_BLACK);
     }
 
+    // Display the playing time
+    tft.setCursor(20, 0, 2);
+    tft.setTextColor(TFT_BLACK, TFT_WHITE);
+    tft.setTextSize(1);
+    tft.print(millis() / 1000);
+    tft.print("s");
 }
