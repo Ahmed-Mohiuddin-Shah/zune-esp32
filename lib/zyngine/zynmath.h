374bd1449ad6429eafcd79c7f5ed79dbc8b368e5
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2025-04-03 17:25:36 +0500

    chore: add PI and other math constants

--- lib/zyngine/zynmath.h
+++ lib/zyngine/zynmath.h
@@ -4,6 +4,22 @@
 #include <config_user.h>
 #include <math.h>
 
+#ifndef PI
+#define PI 3.14159265358979323846f
+#endif
+
+#ifndef EPSILON
+#define EPSILON 0.000001f
+#endif
+
+#ifndef DEG2RAD
+#define DEG2RAD (PI / 180.0f)
+#endif
+
+#ifndef RAD2DEG
+#define RAD2DEG (180.0f / PI)
+#endif
+
 struct ZVec2i
 {
     int x, y;
